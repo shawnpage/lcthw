708df444cad32ec0ccf5e731076e90029d546a55
Shawn Page <shawn.page@gmail.com>
2018-08-04 11:49:19 -0400

    Initial commit
    Exercises 1 and 3

---
+++ README.md
@@ -0,0 +1,1 @@
+Exercises from Learn C the Hard Way by Zed Shaw
---
+++ ex1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/* This is a comment. */
+
+int main(int argc, char *argv[])
+{
+    int distance = 100;
+
+    // this is also a comment
+
+    for (int i = 0; i < 10; i++)
+        {
+            printf("You are %d miles away.\n", distance + i);
+            
+        }
+
+    return 0;
+}
+
+
